eca47d2d44f6711d2e30b5dc75032c2fd66b9231
Patrick Weaver <pjpwea@gmail.com>
2017-06-19 12:02:27 -0400

    reading from local api

--- server.js
+++ server.js
@@ -4,10 +4,26 @@
 var express = require("express");
 var app = express();
 var hbs = require("hbs");
+var http = require('http');
 
 var port = 8106;
 
-var posts = [];
+var options = {
+  host: "localhost",
+  port: 8000,
+  path: "/data.json"
+};
+
+var api_data = {};
+
+http.get(options, function(res){
+  res.on("data", function(data){
+    api_data = JSON.parse(data);
+  });
+}).on("error", function(error){
+  console.log("Error: " + error);
+});
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -22,17 +38,30 @@ app.get('/', function(req, res) {
     res.locals = {
         title: ": Home",
         list: ["1", "2", "3"],
-        posts: posts
+        posts: api_data.posts
     }
 
     res.render("index");
 });
 
-if (process.env.PORT){
-  port = process.env.PORT;
+if (process.env.ENV === "DEV"){
+  console.log("ENV IS: " + process.env.ENV);
+} else {
+  console.log("ENV IS NOT DEV");
+  if (process.env.PORT){
+    console.log(process.env.PORT);
+    //port = process.env.PORT;
+  }
 }
 
 // listen for requests :)
+
+/*
 var listener = app.listen(port, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port + " or " + port);
 });
+
+*/
+
+app.listen(port);
+console.log('Running on http://localhost:' + port);
