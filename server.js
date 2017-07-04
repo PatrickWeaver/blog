69e49f541a2fe60ae56125658a697306d5beac24
Patrick Weaver <pjpwea@gmail.com>
2017-07-04 16:27:34 -0400

    working with live api from env variable

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ var http = require('http');
 var port = 8106;
 
 var options = {
-  host: "localhost",
-  port: 8000,
-  path: "/data.json"
+  host: process.env.API_URL,
+  port: 80,
+  path: "/posts"
 };
 
 var api_data = {};
@@ -19,44 +19,52 @@ var api_data = {};
 http.get(options, function(res){
   res.on("data", function(data){
     api_data = JSON.parse(data);
+    console.log(new Date());
+    runServer();
   });
 }).on("error", function(error){
   console.log("Error: " + error);
 });
 
+function runServer(){
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// https://www.npmjs.com/package/hbs
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
-hbs.registerPartials(__dirname + '/views/partials');
-
-app.get('/', function(req, res) {
-
-    res.locals = {
-        title: ": Home",
-        list: ["1", "2", "3"],
-        posts: api_data.posts
-    }
-
-    res.render("index");
-});
-
-if (process.env.ENV == "DEV"){
-  console.log("ENV IS: " + process.env.ENV);
-} else {
-  console.log("ENV IS NOT DEV");
-  if (process.env.PORT){
-    console.log("env port: " + process.env.PORT);
-    //port = process.env.PORT;
+  for (i in api_data){
+    console.log(i + ": " + api_data[i]);
   }
+  console.log(new Date());
+
+  // http://expressjs.com/en/starter/static-files.html
+  app.use(express.static('public'));
+
+  // https://www.npmjs.com/package/hbs
+  app.set("view engine", "hbs");
+  app.set("views", __dirname + "/views");
+  hbs.registerPartials(__dirname + '/views/partials');
+
+  app.get('/', function(req, res) {
+
+      res.locals = {
+          title: "",
+          posts: api_data
+      }
+
+      res.render("index");
+  });
+
+  if (process.env.ENV == "DEV"){
+    console.log("ENV IS: " + process.env.ENV);
+  } else {
+    console.log("ENV IS NOT DEV");
+    if (process.env.PORT){
+      console.log("env port: " + process.env.PORT);
+      //port = process.env.PORT;
+    }
+  }
+
+  // listen for requests :)
+
+
+  var listener = app.listen(port, function () {
+    console.log('Your app is listening on port ' + listener.address().port + " or " + port);
+  });
 }
-
-// listen for requests :)
-
-
-var listener = app.listen(port, function () {
-  console.log('Your app is listening on port ' + listener.address().port + " or " + port);
-});
