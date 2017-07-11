3505813e2d1b2b89174d8cd63faad24f6a474c19
Patrick Weaver <pjpwea@gmail.com>
2017-07-11 09:24:12 -0400

    working on new post post request

--- public/client.js
+++ public/client.js
@@ -1,24 +1,41 @@
 $('document').ready(function(){
 
-  $(function() {
-    function postAPI(path) {
-      apiUrl = $( "#apiUrl" ).html();
-      alert(apiUrl);
+  console.log("TESTETESTSETSTT");
+
+  $( "#new-post-form .submit").click(function() {
+    postAPI("/blog/posts/new/");
+  })
+
+
+  function postAPI(path) {
+    console.log("TEST");
+    // API url is passed from environment variable to front end via local
+    var apiUrl = $( "#apiUrl" ).html();
+    // Grab post data from the form:
+    var post_title = $( "#new-post-title" ).val();
+    var post_date = $( "#new-post-date" ).val();
+    var post_body = $( "#new-post-body" ).val();
+    var apiData = {
+      post_title: post_title,
+      post_date: post_date,
+      post_body: post_body
     }
-
-    postAPI("hi");
-
-
-
-    $.post("", function() {
-
+    console.log("API URL: " + apiUrl);
+    $.ajax({
+      type: "POST",
+      url: apiUrl + path,
+      data: apiData,
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(err) {
+        console.log("Error: " + err);
+        for (e in err){
+          console.log(e + ": " + err[e]);
+        }
+      },
+      dataType: JSON
     });
-
-    $.post( "ajax/test.html", function( data ) {
-      $( ".result" ).html( data );
-    });
-
-  });
-
+  }
 
 });
