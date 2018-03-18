a8bb4d2233f19c19de77d46e4a916d6b0e064a28
Patrick Weaver <pjpwea@gmail.com>
2018-03-17 21:36:14 -0400

    update apiRequest function for formdata

--- helpers/api.js
+++ helpers/api.js
@@ -1,15 +1,27 @@
 const rp = require("request-promise");
 
-function apiRequest({url, method = "GET", query = "", body = {}}) {
+function apiRequest({url, method = "GET", query = "", body = {}, formData = {}}) {
   var options = {
     url: url,
     method: method,
     qs: query
   }
-  if ((method === "POST" || method === "PUT") && body != {}) {
-    options.body = JSON.stringify(body);
+  if (method === "GET" || method === "DELETE"){
+    return Promise.all([rp(options)]);
+  } else if (method === "POST" || method === "PUT") {
+    if (body != {}) {
+      options.body = JSON.stringify(body);
+      return Promise.all([rp(options)]);
+    } else if (formData != {}) {
+      return Promise.all([rp(options, formData)]);
+    } else {
+      console.log("ELSEING!")
+      return Promise.reject("Not new post, edit post, delete post or file upload");
+    }
+  } else {
+    return Promise.reject("Not GET, POST, PUT or DELETE");
   }
-  return Promise.all([rp(options)]);
+
 }
 
 module.exports = {
